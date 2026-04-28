7bae65730f82b74bb32807118d822f6232ccd801
Graciel Jacinto <gracieljacintojaime85@gmail.com>
2026-04-28 15:54:21 +0200

    Criação da UI-Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import './App.css'
+import './components/LoginForm'
+import LoginForm from './components/LoginForm'
+
 
 function App() {
 
   return (
     <>
-      <h1>Login</h1>
-
-      <form className="form">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-
-        <button type="button">Entrar</button>
-      </form>
+     <LoginForm />
+    
+    
     </>
   )
 }
