514c7cd9568b16a104ec3476558e743f8a747c86
Graciel Jacinto <gracieljacintojaime85@gmail.com>
2026-05-01 00:18:46 +0200

    Pagina de detalhes de eventos

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import './index.css'
 import LoginForm from './pages/Login/LoginForm'
 import Eventdetails from './pages/Login/event-details'
-import  NavBar  from "./components/NavBar";
+
 
 
 
@@ -12,7 +12,7 @@ function App() {
     <>
       {/* <LoginForm /> */}
       <Eventdetails />
-      <NavBar />
+     
     </>
   )
 }
