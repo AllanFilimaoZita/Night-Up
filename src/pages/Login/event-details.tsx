b4c4271ab1299e51bd3fc6a3cc80088b393d90c8
Graciel Jacinto <gracieljacintojaime85@gmail.com>
2026-04-30 19:01:01 +0200

    feat: adicionar imagens ao projeto

--- src/pages/Login/event-details.tsx
+++ src/pages/Login/event-details.tsx
@@ -1,21 +1,38 @@
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
+import capa2 from "../../assets/img/capa2.jpeg";
 
 function EventDetails() {
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
 
-            <div className="w-screen h-screen bg-gray-900 ">
+      <div className="w-screen h-screen bg-white fiexed justify-center items-center">
 
-                <div className="">
-<img src="./" alt="" />
+        <div className="h-1/3 ">
+          <img
+            className="h-full w-full object-top object-cover"
+            src={capa2}
+            alt="capa01"
+          />
+        </div>
 
-                </div>
+
+        <div className="border-2 w-[95%] justify-center items-center flex flex-row h-screen overflow-y-scroll fixed top-60 left-0 p-3 bg-white">
+
+            <div className="z-50 absolute ">
+
+                <h1>Noite do adoço</h1>
 
             </div>
-        </>
-    );
+
+
+
+
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default EventDetails;
