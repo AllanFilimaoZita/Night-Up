338fa995eba4add834eb1774de735090dd76d295
Allan Filimao Zita <allanfilimaozita@gmail.com>
2026-05-05 18:08:57 +0200

    feature/profile: fix spacing gaps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-// import Profile from './pages/Profile/profile'
-import Home from './pages/Home/Home';
+import Profile from './pages/Profile/profile'
+// import Home from './pages/Home/Home';
 
 function App() {
   return (
     <>
-      {/* <Profile /> */}
-      <Home />
+      <Profile />
+      {/* <Home /> */}
     </>
   )
 }
