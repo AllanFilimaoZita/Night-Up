9d4b39845488c26b7b9c7136de3ae06071caa065
Allan Filimao Zita <allanfilimaozita@gmail.com>
2026-05-11 16:51:14 +0200

    feat: create Payment page with order summary and payment method selection

--- src/pages/Payment/Payment.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { MOCK_EVENTS } from '../../data/events';
-import { Header } from '../../components/Header';
-import { NavBar } from '../../components/NavBar';
-
