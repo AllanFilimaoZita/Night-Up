e1a2acd12484114405e228e8d9eeadc52bfca7cc
Graciel Jacinto <gracieljacintojaime85@gmail.com>
2026-05-11 22:32:39 +0200

    updata data

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -1,5 +1,13 @@
+import Header from "/Users/gracieljacinto/Documents/BACKUP/ENSINO DEV/ESTUDOS DEV/WEBDEVSTAR/app/Night-Up/src/components/Header.tsx";
+
+
 function Favorites() {
-  return <h1>FAVORITES OK</h1>;
+  return (
+    <>
+      <Header />
+      <h1>FAVORITES OK</h1>
+    </>
+  );
 }
 
 export default Favorites;
